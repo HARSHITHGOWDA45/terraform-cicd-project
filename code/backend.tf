0afcb9a2ef23b24cdf2eb1fda57688f4ce84eefd
Harshith <harshithrohith45@gmail.com>
2026-03-31 23:00:44 +0530

    adding to remote repo

---
+++ code/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "harshith-ex1-s3"
+    key          = "harshith-s3-ex/test/terraform.tfstate"
+    region       = "ap-south-1" # Replace with your region
+    use_lockfile = true        # S3 Native Locking (No DynamoDB needed)
+    encrypt      = true
+  }
+}
