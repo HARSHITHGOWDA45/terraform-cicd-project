2a02039daf87b7c4172ba6490fab6a858cb9cd09
Harshith <harshithrohith45@gmail.com>
2026-04-01 20:26:33 +0530

    fix: terraform formatting

--- code/backend.tf
+++ code/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket       = "harshith-ex1-s3"
     key          = "harshith-s3-ex/test/terraform.tfstate"
     region       = "ap-south-1" # Replace with your region
-    use_lockfile = true        # S3 Native Locking (No DynamoDB needed)
+    use_lockfile = true         # S3 Native Locking (No DynamoDB needed)
     encrypt      = true
   }
 }
